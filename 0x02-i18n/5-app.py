051719791554b2036978891ed8a36a9085681a0b
Huy Nguyen <huy.nguyen@holbertonschool.com>
2021-08-18 15:00:55 +0000

    feat tasks 3 & 4

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 """ Route module for the API """
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
+from typing import Union
+from os import getenv
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -16,16 +26,16 @@ class Config(object):
 
 
 # set the above class object as the configuration for the app
-app.config.from_object('4-app.Config')
+app.config.from_object('5-app.Config')
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
     """ GET /
     Return:
-      - 4-index.html
+      - 5-index.html
     """
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 @babel.localeselector
@@ -38,6 +48,22 @@ def get_locale() -> str:
             return locale
     else:
         return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user() -> Union[dict, None]:
+    """ Returns user dict if ID can be found """
+    if request.args.get('login_as'):
+        # have to type cast  the param to be able to search the user dict
+        user = int(request.args.get('login_as'))
+        if user in users:
+            return users.get(user)
+    else:
+        return None
+
+@app.before_request
+def before_request():
+    """ Finds user and sets as global on flask.g.user """
+    g.user = get_user()
 
 
 if __name__ == "__main__":
